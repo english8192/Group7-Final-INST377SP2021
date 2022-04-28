b8b95e314d17e4a6163228e750233d0ac9a50902
KrehlK <krehlkasayan@gmail.com>
2022-04-28 11:43:58 -0400

    Finished IC2

--- server/routes/kkRoutes.js
+++ server/routes/kkRoutes.js
@@ -7,15 +7,74 @@ import db from '../database/initializeDB.js';
 const foodRestrictions = `select * from meal_restrictions`;
 const router = express.Router();
 
-router.route('/').get(async (req, res) => {
+router.route('/')
+  .get(async (req, res) => {
+    try {
+      const restrictions = await db.sequelizeDB.query(foodRestrictions, {
+        type: sequelize.QueryTypes.SELECT
+      });
+
+      res.json(restrictions);
+    } catch (err) {
+      res.json({ message: 'Error has occured' });
+    }
+  })
+router.post('/post', async (req, res) => {
+  // TODO original: Error: Table
+  // 'Dining_Hall_Tracker.Meals_Locations' doesn't exist
+
+  // const postQuery = `INSERT INTO meal_restrictions VALUES('${req.query['new_meal_id']}')`;
+
+  const postQuery = `INSERT INTO dietary_restrictions(restriction_id) VALUE ('${req.query['new_rest_id']}');`;
+  const postQuerySecond = `INSERT INTO dietary_restrictions VALUES ('${req.query['new_meal_id']}');`;
+
   try {
-    const restrictions = await db.sequelizeDB.query(foodRestrictions, {
-      type: sequelize.QueryTypes.SELECT
+    const result = await db.sequelizeDB.query(postQuery, {
+      type: sequelize.QueryTypes.POST
     });
-    
-    res.json(restrictions);
+    res.json(`Posted a new row entry with new value:  ${req.query['new_rest_id']}`);
+
+    const resultSecond = await db.sequelizeDB.query(postQuerySecond, {
+      type: sequelize.QueryTypes.POST
+    });
+    res.json(`Posted a new row entry with new value:  ${req.query['new_meal_id']}`);
   } catch (err) {
-    res.json({ message: 'Error has occured' });
+    console.log(err);
+    res.send(err);
+  }
+});
+router.delete('/delete', async (req, res) => {
+  const deleteQuery =
+    `DELETE FROM dietary_restrictions WHERE restriction_id = ${req.query['restriction_id']} ;`;
+  try {
+    const result = await db.sequelizeDB.query(deleteQuery, {
+      type: sequelize.QueryTypes.DELETE
+    });
+
+    res.json(`Deleted the new row entry with meal id:  ${req.query['restriction_id']}`);
+  } catch (e) {
+    console.log(`The following error has occured ${e}`);
+    res.send(`Result could not be furfilled because of ${e}`);
+  }
+});
+router.put('/update', async (req, res) => {
+  const updateQuery =
+
+    `   
+      UPDATE dietary_restrictions
+      SET restriction_type = '${req.query['new_restriction_type']}'
+      WHERE restriction_id = '${req.query['restriction_id']}'
+      `;
+  try {
+    const result = await db.sequelizeDB.query(updateQuery, {
+
+      type: sequelize.QueryTypes.UPDATE
+    });
+
+    res.json(`Updated the row entry ${req.query['restriction_id']} with new value: ${req.query['restriction_id']}`);
+  } catch (e) {
+    console.log(e);
+    res.send(e);
   }
 });
 
